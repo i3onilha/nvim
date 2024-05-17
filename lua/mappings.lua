6f4765439603153185ec772bdfb413146f0f5f87
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2024-05-16 21:17:26 -0400

    fix miss variable

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,6 +3,7 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local default_opts = { noremap = true, silent = true }
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
