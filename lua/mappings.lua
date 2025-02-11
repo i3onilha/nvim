a29812d2bcbad058073096fee41ff8d4eaafe577
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2025-02-11 12:38:14 -0400

    add setup to test debug

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,6 +3,9 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+vim.g.go_debug_windows = {
+    vars = "botright vnew"
+}
 local default_opts = { noremap = true, silent = true }
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
